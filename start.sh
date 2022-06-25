1a87833197a53aedb1baabac64784859e1d98990
root <root@localhost.localdomain>
2022-06-25 11:45:44 +0100

    Update download path for OMV6

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -p 9980:9980 -d -v /srv/1b4e0ce9-da73-412b-bd6a-692a682543f8/Media/Downloads/ytdl:/usr/src/app/ytdl -v /etc/localtime:/etc/localtime:ro --name ytdlpage antfurn/ytdl-page-app
+docker run -p 9980:9980 -d -v /srv/mergerfs/FastPool/Media/Downloads/ytdl:/usr/src/app/ytdl -v /etc/localtime:/etc/localtime:ro --name ytdlpage antfurn/ytdl-page-app
